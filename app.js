3c6a21e2044aabd98b1412cfe7991feddecd3dc6
NyuukLaptop <dnanfab@gmail.com>
2023-08-11 13:57:07 +0700

    Changes to be committed: 	new file:   .gitignore 	new file:   app.js 	new file:   package.json 	new file:   pnpm-lock.yaml

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000; // Ganti dengan port yang Anda inginkan
+
+app.use(bodyParser.json());
+
+const webhookData = [];
+
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+
+  // Menyimpan payload ke dalam array
+  webhookData.push(payload);
+
+  console.log('Received webhook payload:', payload);
+  res.status(200).json({ message: 'Webhook received successfully' });
+});
+
+app.get('/webhook', (req, res) => {
+  res.status(200).json(webhookData);
+});
+
+app.delete('/webhook/:index', (req, res) => {
+  const index = req.params.index;
+
+  if (index < 0 || index >= webhookData.length) {
+    return res.status(400).json({ error: 'Invalid index' });
+  }
+
+  webhookData.splice(index, 1);
+  res.status(200).json({ message: 'Data deleted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
